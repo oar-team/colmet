a3e9993e27262e44d1cc9943c392e58aa9c696ac
Bruno Bzeznik <Bruno.Bzeznik@imag.fr>
2015-03-10 17:05:28 +0100

    [colmet_web] Added io graphs and a template

--- graph/web/colmet_web.py
+++ graph/web/colmet_web.py
@@ -2,12 +2,13 @@
 # Launching:
 #     python colmet_web.py
 # Using:
-#     Point a browser to http://localhost:5000/graph/job/<job_id>/cpu
+#     Point a browser to http://localhost:5000/graph/job/<job_id>
 #
 
 from flask import Flask
 app = Flask(__name__)
 from flask import request
+from flask import render_template
 import sys
 import h5py
 import numpy as np
@@ -48,45 +49,58 @@ def compute_cumul(data):
         v_prev=value
     return out_data
 
-# Cpu/mem graphs
-@app.route('/graph/job/<int:id>/cpu')
-def graph_cpu(id):
+# Cpu/mem/io graphs
+@app.route('/graph/job/<int:id>')
+def graph(id):
+    # Read parameters
     t_res=int(request.args.get('t_res', '1'))
     t_min=int(request.args.get('t_min', '0'))
     t_max=int(request.args.get('t_max', '7200'))
     # Prepare graphs
-    fig_cpu = plt.figure()
-    fig_mem = plt.figure()
-    cpu = fig_cpu.add_subplot(111)
-    mem = fig_mem.add_subplot(111)
+    fig={}
+    graph={}
+    for g in [ 'cpu','mem','read','write']:
+        fig[g] = plt.figure(figsize=(9,5))
+        graph[g] = fig[g].add_subplot(111)
     # Load the metrics
-    metrics=f['job_'+str(id)+'/metrics']['timestamp','hostname','ac_etime','cpu_run_real_total','coremem']
+    metrics=f['job_'+str(id)+'/metrics']['timestamp','hostname','ac_etime','cpu_run_real_total','coremem','read_bytes','write_bytes']
     # Set the origin timestamp
     origin=metrics[0][0]
     # Get a list of uniq hosts
     hosts = set([a[1] for a in metrics])
-    # For each host, create a graph of the cpu load percentagea and memory usage
+    # For each host
     for host in hosts:
       # X axis is a number of seconds starting at 0
       x=[ a[0] for a in metrics if a[1] == host and t_min < a[0]-origin < t_max ]
       x0=x[0]
       x=[ (a - x0)/t_res for a in x ]
-      # Y is the cpu and memory usage
-      y_cpu=[ a[3]/a[2] for a in metrics if a[1] == host and t_min < a[0]-origin < t_max ]
-      #y_mem=[ a[4]/a[2] for a in metrics if a[1] == host and t_min < a[0]-origin < t_max ]
-      y_mem={ a[2]: a[4] for a in metrics if a[1] == host and t_min < a[0]-origin < t_max }
-      # compute cumulative data
+      # Cpu
+      y_cpu=[ 1.0*a[3]/a[2]/1000 for a in metrics if a[1] == host and t_min < a[0]-origin < t_max ]
+      graph['cpu'].plot(x,y_cpu,label=host,lw=5,alpha=0.4)
+      # Memory
+      y_mem={ a[2]: 1.0*a[4]/1024 for a in metrics if a[1] == host and t_min < a[0]-origin < t_max }
       y_mem=compute_cumul(y_mem)
-      # Create the graphs
-      cpu.plot(x,y_cpu,label=host,lw=5,alpha=0.4)
-      mem.plot(x,y_mem,label=host,lw=5,alpha=0.4)
-    # Print a legend
-    cpu.legend()
-    mem.legend()
-    cpu.grid(color='lightgray', alpha=0.7)
-    mem.grid(color='lightgray', alpha=0.7)
-    # Return the figure 
-    return mpld3.fig_to_html(fig_cpu)+mpld3.fig_to_html(fig_mem)
+      graph['mem'].plot(x,y_mem,label=host,lw=5,alpha=0.4)
+      graph['mem'].set_ylabel('rss (GBytes)')
+      # Read/Write
+      y_read={ a[0]: 1.0*a[5]/1024/1024 for a in metrics if a[1] == host and t_min < a[0]-origin < t_max }
+      y_read=compute_cumul(y_read)
+      graph['read'].plot(x,y_read,label=host,lw=5,alpha=0.4)
+      graph['read'].set_ylabel('Read (MBytes/s)')
+      y_write={ a[0]: 1.0*a[6]/1024/1024 for a in metrics if a[1] == host and t_min < a[0]-origin < t_max }
+      y_write=compute_cumul(y_write)
+      graph['write'].plot(x,y_write,label=host,lw=5,alpha=0.4)
+      graph['write'].set_ylabel('Write (MBytes/s)')
+    # Print a legend and generate html
+    html={}
+    for g in [ 'cpu','mem','read','write']:
+        graph[g].legend()
+        graph[g].grid(color='lightgray', alpha=0.7)
+        graph[g].set_xlabel('time (s)')
+        html[g]=mpld3.fig_to_html(fig[g])
+    # Rendering 
+    return render_template('show_job_graphs.html', graph=html)
+    #return mpld3.fig_to_html(fig['cpu'])+mpld3.fig_to_html(fig['mem'])+mpld3.fig_to_html(fig['read'])+mpld3.fig_to_html(fig['write'])
 
 if __name__ == '__main__':
     app.run(debug=True)
