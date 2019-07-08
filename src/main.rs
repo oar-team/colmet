fd11a7046111b3c7ec3f59050aa93dd62f131d47
lambertrocher <lambert.rocher@gmail.com>
2019-07-08 14:16:32 +0200

    user options with cli

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,41 @@ extern crate serde;
 extern crate rmp_serde as rmps;
 extern crate zmq;
 
+#[macro_use]
+extern crate clap;
+use clap::App;
+
+use std::thread;
+use std::time::Duration;
+
 mod stdout;
 
-use std::collections::HashMap;
-use serde::{Deserialize};
-use rmps::{Deserializer};
+
 
 fn main() {
 
-    use std::thread;
-    use std::time::Duration;
+    // parse arguments from command line interface
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+//
+//    let config = matches.value_of("config").unwrap();
+//    println!("Value for config: {}", config);
 
+    let sample_period = value_t!(matches, "sample-period", f64).unwrap();
+    println!("Value for sampling : {}", sample_period);
+
+    let stdout = value_t!(matches, "enable-stdout-backend", bool).unwrap();
+    println!("Value for stdout : {}", stdout);
+
+
+    // receive data using zeromq
     let context = zmq::Context::new();
     let receiver = context.socket(zmq::PULL).unwrap();
     assert!(receiver.bind("tcp://0.0.0.0:5556").is_ok());
 
     loop {
         match receiver.recv_bytes(1) {
-            Ok(x) => stdout::display_metrics(x),
+            Ok(x) => stdout::push(x), // push data to backend
             Err(e) => println!("error {}", e),
         };
 
--- src/stdout.rs
+++ src/stdout.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use serde::{Deserialize};
 use rmps::{Deserializer};
 
-pub fn display_metrics(x: Vec<u8>){
+pub fn push(x: Vec<u8>){
     let mut de = Deserializer::new(&x[..]);
     let res : Vec<(String, String, u64, HashMap<String, HashMap<String, u64>>)> = Deserialize::deserialize(&mut de).unwrap();
 
